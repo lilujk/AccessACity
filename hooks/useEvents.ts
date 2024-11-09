5008af4d4696880dd9347bf28dc0bf5fc99c1eb6
lilujk <kaufmanliluj@gmail.com>
2024-11-09 18:01:19 -0500

    change event object formatting

--- hooks/useEvents.ts
+++ hooks/useEvents.ts
@@ -12,14 +12,25 @@ import { createContext, useContext } from 'react';
 */
 export const EventsContext = createContext([
     {
-        Name: "fakename",
-        Description: "fakedescription",
-        Tags: ["faketag1", "faketag2"],
-    },
-    {
-        Name: "fakename2",
-        Description: "fakedescription2",
-        Tags: ["faketag3", "faketag4"],
+        username: "testusername",
+        eventname: "testeventname",
+        description: "testdescription",
+        eventcity: "testeventcity",
+        eventstate: "testeventstate",
+        eventdate: "11/16/2024",
+        eventtime: "09:00AM - 01:00 PM",
+        tags: ["testtag1", "testtag2", "testtag3"],
+        cost: "free",
+        accessibility: {
+            wheelchair: "high",
+            sight: "low to none",
+            sound: "med",
+            touch: "low to none",
+            smell: "med"
+        },
+        xcord: 1,
+        ycord: 1,
+        rsvp: "test@email.com"
     }
 ]);
 
